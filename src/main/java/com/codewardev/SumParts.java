4d00644e420c24f9f59d2781e6660934ccdd49f0
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-04 21:42:09 +1100

    refined working cut

--- src/main/java/com/codewardev/SumParts.java
+++ src/main/java/com/codewardev/SumParts.java
@@ -9,7 +9,7 @@ public class SumParts {
 	public static int[] sumParts(int[] ls) {
 		int[] result = new int[ls.length+1];
 		result[0] = Arrays.stream(ls).sum();
-		for(int i=1; i<ls.length+1; i++) {
+		for(int i=1; i<=ls.length; i++) {
 			result[i] = result[i-1] - ls[i-1];
 		}
 
